c69cdbf68b558a109fc35e2100ea4c7df97d77d6
Lens Kamdem <kamdem.lens@gmail.com>
2024-06-07 20:10:11 +0000

    Added endpoint for file upload

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var cors = require('cors');
-require('dotenv').config()
+const multer = require('multer');
+const path = require('path');
+require('dotenv').config();
 
 var app = express();
+const upload = multer({ dest: 'uploads/' });
+
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,7 +15,15 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+// POST endpoint to handle file upload
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  const { originalname, mimetype, size } = req.file;
+  res.json({
+    name: originalname,
+    type: mimetype,
+    size: size
+  });
+});
 
 
 const port = process.env.PORT || 3000;
